3c201adf601115f75a54324a38991efdce84e2c9
CharlesBreedlove <charleslewisbreedlove@gmail.com>
2018-04-28 08:27:06 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "p7hBHDRVoXbgvxKaXycHa85iR"
+consumer_secret = "vJCavvkW1THzwZu91lkaqLFwbYJwh4gM7acLqAOSlgdVskXTND"
+access_token = "986021521310314496-lTD4YlUt9Zw3oUcjlpnCDNPiHmNYoNN"
+access_token_secret = "LyXpIsTcAbO0unHpmaRbFujPoIusIW4cWsMfUD7ysJ0Co"
+
